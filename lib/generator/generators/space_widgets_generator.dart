333dcbbb087cbaf8ca3b1b09c185d994c19a2d94
Pierre2tm <pmtothemoon@gmail.com>
2021-11-13 19:53:56 +0100

    space widget now inherit from SizedBox

--- lib/generator/generators/space_widgets_generator.dart
+++ lib/generator/generators/space_widgets_generator.dart
@@ -4,23 +4,14 @@ import 'package:source_gen/source_gen.dart';
 import 'package:neat/generator/generator_options.dart';
 import 'class_const_field_parser.dart';
 
-//TODO: better space generator
-//
-//class Space1 extends SizedBox {
-//  const Space1({Key? key}) : super(height: 1, width: 1, key: key);
-//  const Space1.w({Key? key}) : super(height: 0, width: 1, key: key);
-//  const Space1.h({Key? key}) : super(height: 1, width: 0, key: key);
-//}
-
 class SpaceWidgetsGenerator extends GeneratorForAnnotation<NeatSpaces>
     with ClassConstFieldParser {
   @override
   String generateForAnnotatedElement(
     Element element,
     ConstantReader annotation,
-    __,
+    _,
   ) {
-    //TODO: parse config
     return parseElementAsClass(element);
   }
 
@@ -31,10 +22,11 @@ class SpaceWidgetsGenerator extends GeneratorForAnnotation<NeatSpaces>
     double space,
   ) {
     final className = options.generateWidgetName("Space", field);
+    final s = space.toStringAsFixed(0);
     final widgetCode = Class(
       (b) => b
         ..name = className
-        ..extend = refer('StatelessWidget')
+        ..extend = refer('SizedBox')
         ..constructors.addAll([
           Constructor(
             (c) => c
@@ -46,11 +38,9 @@ class SpaceWidgetsGenerator extends GeneratorForAnnotation<NeatSpaces>
                     ..type = Reference("Key?"),
                 ),
               ])
-              ..initializers.addAll([
-                Code("width = ${space.toStringAsFixed(0)}"),
-                Code("height = ${space.toStringAsFixed(0)}"),
-                Code("super(key: key)"),
-              ])
+              ..initializers.add(
+                Code("super(height: $s, width: $s, key: key,)"),
+              )
               ..constant = true,
           ),
           Constructor(
@@ -64,11 +54,9 @@ class SpaceWidgetsGenerator extends GeneratorForAnnotation<NeatSpaces>
                     ..type = Reference("Key?"),
                 ),
               ])
-              ..initializers.addAll([
-                Code("width = ${space.toStringAsFixed(0)}"),
-                Code("height = 0"),
-                Code("super(key: key)"),
-              ])
+              ..initializers.add(
+                Code("super(height: 0, width: $s, key: key,)"),
+              )
               ..constant = true,
           ),
           Constructor(
@@ -82,46 +70,12 @@ class SpaceWidgetsGenerator extends GeneratorForAnnotation<NeatSpaces>
                     ..type = Reference("Key?"),
                 ),
               ])
-              ..initializers.addAll([
-                Code("width = 0"),
-                Code("height = ${space.toStringAsFixed(0)}"),
-                Code("super(key: key)"),
-              ])
+              ..initializers.add(
+                Code("super(height: $s, width: 0, key: key,)"),
+              )
               ..constant = true,
           ),
-        ])
-        ..fields.addAll([
-          Field(
-            (f) => f
-              ..name = "width"
-              ..modifier = FieldModifier.final$
-              ..type = refer("double"),
-          ),
-          Field(
-            (f) => f
-              ..name = "height"
-              ..modifier = FieldModifier.final$
-              ..type = refer("double"),
-          )
-        ])
-        ..methods.add(
-          Method(
-            (m) => m
-              ..annotations.add(refer("override"))
-              ..returns = refer('Widget')
-              ..name = "build"
-              ..body = const Code(
-                "return SizedBox(width: width, height: height);",
-              )
-              ..requiredParameters.add(
-                Parameter(
-                  (p) => p
-                    ..type = refer("BuildContext")
-                    ..name = "context",
-                ),
-              ),
-          ),
-        ),
+        ]),
     );
     final emitter = DartEmitter();
     return '${widgetCode.accept(emitter)}';
