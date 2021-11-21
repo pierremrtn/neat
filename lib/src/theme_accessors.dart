fa315e1021553a990e8fb5479836ced2531f28b9
Pierre2tm <pmtothemoon@gmail.com>
2021-11-21 17:46:31 +0100

    plural

--- lib/src/theme_accessors.dart
+++ lib/src/theme_accessors.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-extension NeatThemeAccessor on BuildContext {
+extension NeatThemeAccessors on BuildContext {
   ThemeData get theme => Theme.of(this);
   TextTheme get textTheme => Theme.of(this).textTheme;
   ColorScheme get colorScheme => Theme.of(this).colorScheme;
